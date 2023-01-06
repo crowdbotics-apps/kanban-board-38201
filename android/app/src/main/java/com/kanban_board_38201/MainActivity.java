e2504980b52ef3d4a8e68d5fe7fc58019740c642
Crowdbotics <team@crowdbotics.com>
2023-01-06 08:27:54 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/kanban_board_38201/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kanban_board_38201;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "kanban_board_38201";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "kanban-board-38201",
+  url: "https://kanban-board-38201.botics.co",
+  api: "https://kanban-board-38201.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
